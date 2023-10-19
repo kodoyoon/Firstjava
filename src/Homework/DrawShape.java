ff0ebac67bc72ea88de040bab6c6238f1ecb3488
doyoon <doyoun0527@naver.com>
2023-10-19 17:09:42 +0900

    변경

---
+++ src/Homework/DrawShape.java
@@ -0,0 +1,13 @@
+package Homework;
+
+import java.awt.*;
+
+public class DrawShape {
+  public static void main(String[] args) {
+    Point[] P = {
+        new Point(100, 100),
+        new Point(140, 50),
+        new Point(200, 100)
+    };
+  }
+}
