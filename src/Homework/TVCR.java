04d5da5113af2d841e50257c3928bbf55f8f2713
doyoon <doyoun0527@naver.com>
2023-10-19 19:17:20 +0900

    변경

--- src/Homework/TVCR.java
+++ src/Homework/TVCR.java
@@ -1,4 +1,17 @@
 package Homework;
 
 public class TVCR {
+  VCR vcr = new VCR();
+  void play () {
+    vcr.play();
+  }
+  void stop() {
+    vcr.stop();
+  }
+  void rew() {
+    vcr.rew();
+  }
+  void ff() {
+    vcr.ff();
+  }
 }
