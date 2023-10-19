79fce028af71e22d4b49785cd21f2a8fd6d3e951
doyoon <doyoun0527@naver.com>
2023-10-19 14:48:51 +0900

    변경

---
+++ src/Homework/Cardtest.java
@@ -0,0 +1,19 @@
+package Homework;
+
+public class Cardtest {
+  public static void main(String[] args) {
+    System.out.println("Card.width = " + Card.width);
+    System.out.println("Card.height = " + Card.height);
+
+    Card c1 = new Card();
+    c1.kind = "Heart";
+    c1.number = 7;
+
+    Card c2 = new Card();
+    c2.kind = "Spade";
+    c2.number = 4;
+
+
+
+  }
+}
