a8e3d0c63fb6463764566334c892e28bbdd9185e
doyoon <doyoun0527@naver.com>
2023-10-20 15:32:47 +0900

    변경

--- src/SoloProject/Purchase.java
+++ src/SoloProject/Purchase.java
@@ -15,10 +15,13 @@ public class Purchase {
     System.out.println("아래 메뉴판을 보시고 메뉴를 골라 입력해주세요.");
 
     System.out.println("[SHAKESHACK MENU]");
-    System.out.println();
-    System.out.println();
-    System.out.println();
-    System.out.println();
+    System.out.println("1.Burgers           | 앵거스 비프 통살을 다녀만든 버거");
+    System.out.println("2.Forzen Custard    | 매장에서 신선하게 만드는 아이스크림");
+    System.out.println("3.Drinks            | 매장에서 직접 만드는 음료");
+    System.out.println("4.Beer              | 뉴욕 브루쿨린 브루어리에서 양조한 맥주");
 
+    System.out.println("[ORDER MENU]");
+    System.out.println("5.Order  | 장바구니를 확인 후 주문합니다.");
+    System.out.println("6.Cancel | 진행중인 주문을 취소합니다.");
   }
 }
