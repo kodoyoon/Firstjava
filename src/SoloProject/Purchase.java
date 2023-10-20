b863edc9a35a23cba378fea9d5c12ad84699e33b
doyoon <doyoun0527@naver.com>
2023-10-20 15:21:53 +0900

    변경

--- src/SoloProject/Purchase.java
+++ src/SoloProject/Purchase.java
@@ -1,7 +1,10 @@
 package SoloProject;
 
+import java.util.Scanner;
+
 public class Purchase {
   public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
 
     System.out.println("Hamburger  |W 5.4 | 비프패티를 기반으로 야채가 들어간 기본버거");
     System.out.println("위 메뉴를 장바구니에 추가하시겠습니까?");
