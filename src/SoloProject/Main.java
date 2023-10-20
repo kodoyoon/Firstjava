6dd92e00009af2b0ec9582902e2803d282c03bbd
doyoon <doyoun0527@naver.com>
2023-10-20 14:37:52 +0900

    변경

--- src/SoloProject/Main.java
+++ src/SoloProject/Main.java
@@ -2,6 +2,7 @@ package SoloProject;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println();
+    System.out.println("SHAKESHACK BURGER 에 오신걸 환영합니다.");
+    System.out.println("아래 메뉴판을 보시고 메뉴를 골라 입력해주세요");
   }
 }
