d282f001798fc158086d3cce161d64410dfd22f5
doyoon <doyoun0527@naver.com>
2023-10-20 15:17:32 +0900

    변경

--- src/SoloProject/Main.java
+++ src/SoloProject/Main.java
@@ -2,8 +2,9 @@ import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
+
     Scanner scanner = new Scanner(System.in);
-    int choice;
+   int choice;
 
     System.out.println("SHAKESHACK BURGER 에 오신걸 환영합니다.");
     System.out.println("아래 메뉴판을 보시고 메뉴를 골라 입력해주세요.");
---
+++ src/SoloProject/Purchase.java
@@ -0,0 +1,10 @@
+package SoloProject;
+
+public class Purchase {
+  public static void main(String[] args) {
+    System.out.println("Hamburger  |W 5.4 | 비프패티를 기반으로 야채가 들어간 기본버거");
+    System.out.println("위 메뉴를 장바구니에 추가하시겠습니까?");
+    System.out.println("1. 확인     2. 취소");
+
+  }
+}
