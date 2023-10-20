b5962076736be38729b505a20aba905189afe9b5
doyoon <doyoun0527@naver.com>
2023-10-20 14:54:23 +0900

    변경

--- src/SoloProject/Main.java
+++ src/SoloProject/Main.java
@@ -6,6 +6,7 @@ public class Main {
     int choice;
 
     System.out.println("SHAKESHACK BURGER 에 오신걸 환영합니다.");
+    System.out.println("아래 메뉴판을 보시고 메뉴를 골라 입력해주세요.");
 
     System.out.println("[ SHAKESHACK MENU ]");
     System.out.println("1. Burgers         | 앵거스 비프 통살을 다져만든 버거");
---
+++ src/SoloProject/Menu.java
@@ -0,0 +1,8 @@
+package SoloProject;
+
+public class Menu {
+  public static void main(String[] args) {
+    System.out.println("SHAKESHACK BURGER 에 오신걸 환영합니다.");
+
+  }
+}
