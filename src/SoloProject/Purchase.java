19af13cc04f6689291578c4495fb01584913fcd8
doyoon <doyoun0527@naver.com>
2023-10-20 15:27:49 +0900

    변경

--- src/SoloProject/Purchase.java
+++ src/SoloProject/Purchase.java
@@ -10,9 +10,15 @@ public class Purchase {
     System.out.println("위 메뉴를 장바구니에 추가하시겠습니까?");
     System.out.println("1. 확인     2. 취소");
 
+    System.out.println("Hamburger 가 장바구니에 추가되었습니다.");
+    System.out.println("SHAKESHACK BURGER 에 오신걸 환영합니다.");
+    System.out.println("아래 메뉴판을 보시고 메뉴를 골라 입력해주세요.");
 
-
-
+    System.out.println("[SHAKESHACK MENU]");
+    System.out.println();
+    System.out.println();
+    System.out.println();
+    System.out.println();
 
   }
 }
