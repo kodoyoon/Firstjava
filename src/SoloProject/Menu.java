317e37e2a4c92952eb17bc97b5572bbc51100ee7
doyoon <doyoun0527@naver.com>
2023-10-20 14:55:21 +0900

    변경

--- src/SoloProject/Menu.java
+++ src/SoloProject/Menu.java
@@ -3,6 +3,7 @@ package SoloProject;
 public class Menu {
   public static void main(String[] args) {
     System.out.println("SHAKESHACK BURGER 에 오신걸 환영합니다.");
+    System.out.println("아래 상품메뉴판을 보시고 상품을 골라 입력해주세요.");
 
   }
 }
