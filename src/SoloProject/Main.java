dbe8871c323b7bdc30f297c0e5c66a4d430cac9d
doyoon <doyoun0527@naver.com>
2023-10-20 14:49:13 +0900

    변경

--- src/SoloProject/Main.java
+++ src/SoloProject/Main.java
@@ -17,32 +17,31 @@ public class Main {
       System.out.println("6. Cancel          | 진행중인 주문을 취소합니다.");
 
       System.out.print("메뉴를 선택해주세요: ");
-      choice = scanner.nextInt();
+      choice  = scanner.nextInt();
 
       switch (choice) {
         case 1:
           System.out.println("Burgers를 선택하셨습니다.");
-          // 여기에 버거 주문 로직을 추가하세요.
           break;
+
         case 2:
           System.out.println("Forzen Custard를 선택하셨습니다.");
-          // 여기에 아이스크림 주문 로직을 추가하세요.
           break;
+
         case 3:
           System.out.println("Drinks를 선택하셨습니다.");
-          // 여기에 음료 주문 로직을 추가하세요.
           break;
+
         case 4:
           System.out.println("Beer를 선택하셨습니다.");
-          // 여기에 맥주 주문 로직을 추가하세요.
           break;
+
         case 5:
           System.out.println("Order를 선택하셨습니다.");
-          // 여기에 주문 확인 및 완료 로직을 추가하세요.
           break;
+
         case 6:
           System.out.println("Cancel를 선택하셨습니다.");
-          // 여기에 주문 취소 로직을 추가하세요.
           break;
         default:
           System.out.println("올바른 메뉴를 선택해주세요.");
