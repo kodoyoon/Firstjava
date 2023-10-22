befcffba10f9380303ff772a42f167df15146abe
doyoon <doyoun0527@naver.com>
2023-10-22 16:44:41 +0900

    변경

--- src/SoloProject/Done.java
+++ src/SoloProject/Done.java
@@ -1,4 +1,11 @@
 package SoloProject;
 
 public class Done {
+  public static void main(String[] args) {
+    System.out.println("주문이 완료되었습니다.");
+    System.out.println("대기번호는 [ 1 ] 번 입니다.");
+    System.out.println("(3초후 메뉴판으로 돌아갑니다.)");
+
+
+  }
 }
