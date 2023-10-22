c0f0dc3de1ffdea6e8fc901541b5e91fe4c4b6a2
doyoon <doyoun0527@naver.com>
2023-10-22 16:47:14 +0900

    변경

--- src/SoloProject/Cancel.java
+++ src/SoloProject/Cancel.java
@@ -2,6 +2,9 @@ package SoloProject;
 
 public class Cancel {
   public static void main(String[] args) {
-    System.out.println();
+    System.out.println("진행하던 주문을 취소하시겠습니까?");
+    System.out.println("1. 확인 2. 취소");
+
+
   }
 }
